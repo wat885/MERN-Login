2578a048b0cfaa8838ce1b7d963eb17f4730e8fa
Watcharin Tassananchalee <twatcharin90@gmail.com>
2022-06-01 23:32:06 +0700

    9-login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs')
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 exports.register = async (req, res) => {
   try {
@@ -23,6 +24,44 @@ exports.register = async (req, res) => {
     res.status(500).send("Server Error!");
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const {username, password} = req.body;
+    var user = await User.findOneAndUpdate({username},{new:true})
+    if(user && user.enabled){
+      //Check Password
+      const isMatch = await bcrypt.compare(password, user.password)
+                                          //postman   //ในmongodb
+      if(!isMatch){
+        return res.status(400).send('Password Invalid!!')
+      }
+      //Payload
+      const payload ={
+        user:{
+          username: user.username, //
+          role: user.role
+        }
+      }
+      // Gennerate Token แล้วส่งไปหน้าบ้าน
+      jwt.sign(payload, 'jwtSecret', {expiresIn: 3600} , 
+      (err,token)=>{
+        if(err) throw err
+        res.json({token,payload})
+        // ดูต่อใน react เอา payload มาเพราะอะไร
+        
+      })
+
+
+    }else{
+      return res.status(400).send('User Not Found!!')
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server Error!");
+  }
+};
+
 
 exports.listUser = async (req, res) => {
   try {
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,27 +4,34 @@ const router = express.Router();
 //controller
 const {
   register,
+  login,
   listUser,
   editUser,
   deleteUser,
+  
 } = require("../controllers/auth");
 
-//endpoint //http://localhost:3000/api/auth
+//endpoint http://localhost:3000/api/register
+//method    post ส่งข้อมูล
+//Access    publish
+router.post("/register", register);
+
+//endpoint http://localhost:3000/api/register
+//method    post ส่งข้อมูล
+//Access    publish
+router.post("/login", login);
+
+//endpoint http://localhost:3000/api/auth
 //method    get
 //Access    publish
 router.get("/auth", listUser);
 
-//endpoint //http://localhost:3000/api/auth
-//method    post ส่งข้อมูล
-//Access    publish
-router.post("/auth", register);
-
-//endpoint //http://localhost:3000/api/auth
+//endpoint http://localhost:3000/api/auth
 //method    put  แก้ไข
 //Access    publish
 router.put("/auth", editUser);
 
-//endpoint //http://localhost:3000/api/auth
+//endpoint http://localhost:3000/api/auth
 //method    delete
 //Access    publish
 router.delete("/auth", deleteUser);
